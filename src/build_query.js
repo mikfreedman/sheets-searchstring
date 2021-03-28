7a23f7279b12dfaf6d957c612dc2175c86b7715d
Mik Freedman <github@michael-freedman.com>
2021-03-28 16:40:54 -0400

    make the code a bit more readable

--- src/build_query.js
+++ src/build_query.js
@@ -1,10 +1,12 @@
 export default function BUILD_QUERY(input) {
     if (Array.isArray(input)) {
+        const firstBlankRow = input.findIndex(a => a[2] == "");
         return input
-            .slice(0, input.findIndex(a => a[2] == ""))
+            .slice(0, firstBlankRow > 0 ? firstBlankRow : input.length)
             .filter(a => a[0])
             .map(row => {
-                let predicates = row.slice(2, row.findIndex(a => a == ""));
+                const firstBlankCol = row.findIndex(a => a == "");
+                let predicates = row.slice(2, firstBlankCol > 0 ? firstBlankCol : row.length);
                 let p = predicates.map(p => p.match(/[^\w]/) ? `"${p}"` : p).join(" OR ")
 
                 return predicates.length > 1 ? `(${p})` : p
