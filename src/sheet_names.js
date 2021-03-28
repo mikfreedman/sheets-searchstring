fe920788fd6ff142d1fe23bbac9e3557a384095e
Mik Freedman <github@michael-freedman.com>
2021-03-28 16:40:25 -0400

    remove unused variable

--- src/sheet_names.js
+++ src/sheet_names.js
@@ -1,4 +1,3 @@
  export default function SHEET_NAMES() {
-   let names = [];
    return SpreadsheetApp.getActiveSpreadsheet().getSheets().filter(s => !s.isSheetHidden()).map(s => s.getSheetName())
  }
